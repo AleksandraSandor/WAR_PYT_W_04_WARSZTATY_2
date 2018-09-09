8ef063b5eeb2c0f07f64521b762b48d333c93650
Aleksandra Sandor <aleksandra.walas87@gmail.com>
2018-09-09 15:24:43 +0200

    update

---
+++ connection.py
@@ -0,0 +1,60 @@
+from psycopg2 import connect, OperationalError
+
+def create_connection(db_name):
+    username = "posgres"
+    passwd = "coderslab"
+    hostname = "127.0.0.1"  # lub "localhost"
+    cnx = None
+    try:
+        cnx = connect(user=username, password=passwd, host=hostname, database=db_name)
+        cnx.autocommit = True #informacja o tym, że to jest transakcja
+        print("Połączenie udane.")
+    except OperationalError as error:
+        print(error)
+        print("Nieudane połączenie.")
+    return cnx
+
+def execute_sql(cnx, sql_file):
+    cursor = cnx.cursor()
+    result = []
+    file = open(sql_file, "r")
+    sql_text = file.read()
+    file.close()
+
+    sqlCommands = sql_text.split(";")
+    print(sqlCommands)
+
+    for command in sqlCommands:
+        if command == "": continue
+        try:
+            cursor.execute(command)
+            #result.append(cursor.fetchall())
+            result.append({"query": command, "result": cursor.fetchall()})
+        except Exception as e:
+            print(e)
+
+    cursor.close()
+    return result
+
+#pojedyncze zapytanie sql
+def execute_single_sql(cnx, command):
+    cursor = cnx.cursor()
+
+    result = None
+    try:
+        cursor.execute(command)
+        result = cursor.fetchall()
+    except Exception as e:
+        print(e)
+
+    cursor.close()
+    return result
+
+if __name__ == "__main__":
+    connection = create_connection("postgres")  #tu nazwa bazy
+    results = execute_sql(connection, "create_base.sql") #tu nazwa pliku, w którym tworzymy zapytanie sql
+
+    for result in results:
+        print(result)
+
+    connection.close()
---
+++ create_base.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE users_db;
+
+CREATE TABLE user
+(
+id serial,
+username varchar(255),
+email varchar(255) unique ,
+password varchar (80),
+PRIMARY KEY(id)
+);
